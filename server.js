3f8b443dddc586c4f053badf193170dd809d2aee
CodeFreq <codefreq@Macintosh-256.local>
2019-04-14 13:16:07 -0700

    Clean up code.

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const app = express();
 const port = 8000;
 const db = require('./app/db');
 
-// const db = {
-//     events: [],
-//     reminders: [],
-//     users: {}
-
-// };
-
 app.use(cors()); // Allow CORS on all routes
 app.use(bodyParser.urlencoded({extended: true})); // Let Express handle URL-encoded forms
 require('./app/routes')(app, db);
